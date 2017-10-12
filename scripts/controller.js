f55a4cf43ac78f8acf666455e8c4396435d8ae60
Amy Scoville <amyscoville@Amys-MacBook-Air.local>
2017-10-12 14:08:02 -0600

    Created files, began writing logic

---
+++ scripts/controller.js
@@ -0,0 +1,10 @@
+(function(){
+    angular
+    .module('madlibs')
+    .controller('madlibsController', madlibsCtrl);
+
+    function madlibsCtrl() {
+        var vm = this;
+        
+    }
+})();
